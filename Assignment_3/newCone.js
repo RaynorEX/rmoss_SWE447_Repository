f56c5c082534fe612fd8ec8cfbf1345d7a6ccc4f
RaynorEX <71462312+RaynorEX@users.noreply.github.com>
2021-05-27 22:57:19 -0700

    assignment 3

---
+++ Assignment_3/newCone.js
@@ -0,0 +1,23 @@
+var gl = null;
+
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+	
+	Cone(gl);
+    
+	render();
+}
+
+function render() {
+	cone.render;
+   ;
+}
+
+window.onload = init();
